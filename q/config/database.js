a282e2768b6a88e8052f5ea5c063b84b332a608d
dshamany <dshamany@gmail.com>
2020-01-29 15:09:59 -0800

    clean up unused code

--- q/config/database.js
+++ q/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost', {
+mongoose.connect(process.env.DATABASE_URL, {
     useCreateIndex: true,
     useFindAndModify: true,
     useNewUrlParser: true,
