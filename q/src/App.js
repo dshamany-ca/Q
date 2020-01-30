5293c3840f8c79f25a2b11fbb308ddd4bbc82dd1
dshamany <dshamany@gmail.com>
2020-01-30 09:15:08 -0800

    add primitive navbar for navigation

--- q/src/App.js
+++ q/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import ExampleComponent from './components/ExampleComponent';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <ExampleComponent />
+      <NavBar navItems={['home', 'about']} />
+      <Switch>
+        <Route exact path="/" component={ExampleComponent} />
+        <Route exact path="/about" render={() => {
+          return(
+            <div>
+              <h1>About</h1>
+            </div>
+          )
+        }} />
+      </Switch>
     </div>
   );
 }
---
+++ q/src/constants/NavBarItems.js
@@ -0,0 +1,12 @@
+export default {
+        home: {
+            title: "Home",
+            path: "/",
+            key: "home",
+        },
+        about: {
+            title: "About",
+            path: "/about",
+            key: "about",
+        }   
+};
